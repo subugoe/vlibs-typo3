e2a0f71a5878e0ab6e9399972849751f66fe4d6a
porst <porst@8419a118-b794-0410-87fc-e17fafa7fe4c>
2010-11-11 09:57:34 +0000

    remove GBV cover art placeholder code
    git-svn-id: https://develop.sub.uni-goettingen.de/repos/vlib/vlib@42 8419a118-b794-0410-87fc-e17fafa7fe4c

--- fileadmin/pazpar2/pz2-client.js
+++ fileadmin/pazpar2/pz2-client.js
@@ -670,48 +670,7 @@ function renderDetails(data, marker) {
 		return linkElement;
 	}
 
-	/* 
-		Create link to bookcover at GBV
-		Requires jQuery
-	*/
-/*
-	var GBVBookLinkForNumber = function (number) {
-		if (id != null && id != "") {
 
-		var url = "http://ws.gbv.de/covers/?format=seealso&callback=?&id=" + number;
-		var coverContainer = document.createElement('span');
-		coverContainer.setAttribute('id', )
-
-		$.getJSON(url, function(response) {
-			if (!response || !response[2][0] || !response[3][0]) return;
-			var imghref = response[3][0];
-			var size = response[2][0].split('x');
-			var width = 36;
-			var height = Math.round(size[1]/size[0]*width);
-			var type = "image";
-			if (response[1][0]) type = response[1][0];
-			if (imghref) { // imghref ist die URL des Covers
-				$("#bookcover").html('<img src="' + imghref + '"' + 
-					'alt="' + type + '"' + 
-					'title="' + type + '"' +
-					'width="' + width + '"' + 'height="' + height + '"' + 
-					'style="width:' + width + ';' + 'height:' + height + '"' +
-					'border="0">'
-				);
-				var img = $("#bookcover > img");
-				img.hover(
-					function(){
-						$(this).attr({'width' : size[0], 'height' : size[1]})
-							.css({'width' : size[0], 'height' : size[1]});
-					}, function(){
-						$(this).attr({'width' : width, 'height': height})
-							.css({'width' : width, 'height' : height});
-					}
-				);
-			}
-		});
-	} 
-*/ 
 
 	/*	DOMElementForTitle
 		input:	title - title string
