4ad57a82d72794a7b1f6ead4b9df3bdf49a77fe9
porst <porst@8419a118-b794-0410-87fc-e17fafa7fe4c>
2011-01-03 19:42:44 +0000

    make pazpar2neuerwerbungen subclass of pazpar2
    remove plenty of duplicate code
    
    git-svn-id: https://develop.sub.uni-goettingen.de/repos/vlib/vlib@153 8419a118-b794-0410-87fc-e17fafa7fe4c

--- typo3conf/ext/pazpar2neuerwerbungen/Classes/Controller/Pazpar2neuerwerbungenController.php
+++ typo3conf/ext/pazpar2neuerwerbungen/Classes/Controller/Pazpar2neuerwerbungenController.php
@@ -1,63 +1,54 @@
 <?php
-/********************************************************************
+/*************************************************************************
  *  Copyright notice
  *
- *  © 2010 Sven-S. Porst, SUB Göttingen <porst@sub.uni-goettingen.de>
- *  All rights reserved
+ *  © 2010-2011 Sven-S. Porst, SUB Göttingen <porst@sub.uni-goettingen.de>
+ *  All rigs reserved
  *
  *  This script is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  *  This copyright notice MUST APPEAR in all copies of the script.
- ********************************************************************/
+ *************************************************************************/
 
 
 /**
  * Pazpar2neuerwerbungenController.php
  *
- * Main controller for pazpar2 Neuerwerbungen plug-in.
+ * Main controller for pazpar2 Neuerwerbungen plug-in,
+ * a subclass of the pazpar2 plug-in.
  *
  * @author Sven-S. Porst <porst@sub-uni-goettingen.de>
  */
 
+require_once(t3lib_extMgm::extPath('pazpar2') . 'Classes/Controller/Pazpar2Controller.php');
 
 
 
 /**
  * Controller for the pazpar2 Neuerwerbungen package.
  */
-class Tx_Pazpar2neuerwerbungen_Controller_Pazpar2neuerwerbungenController extends Tx_Extbase_MVC_Controller_ActionController {
+class Tx_Pazpar2neuerwerbungen_Controller_Pazpar2neuerwerbungenController extends Tx_Pazpar2_Controller_Pazpar2Controller {
 
 	/**
-	 * Initialiser
+	 * defaultSettings: Return array with default settings.
+	 * Add own default settings to those set by the superclass.
 	 *
-	 * @return void
+	 * @return array
 	 */
-	public function initializeAction () {
-		$defaults = array(
-			'serviceID' => '',
-			'CSSPath' => t3lib_extMgm::siteRelPath('pazpar2') . 'Resources/Public/pz2.css',
-			'pz2JSPath' => t3lib_extMgm::siteRelPath('pazpar2') . 'Resources/Public/pz2.js',
-			'pz2-clientJSPath' => t3lib_extMgm::siteRelPath('pazpar2') . 'Resources/Public/pz2-client.js',
-			'pz2-neuerwerbungenJSPath' => t3lib_extMgm::siteRelPath('pazpar2neuerwerbungen') . 'Resources/Public/pz2-neuerwerbungen.js',
-			'pz2-neuerwerbungenCSSPath' => t3lib_extMgm::siteRelPath('pazpar2neuerwerbungen') . 'Resources/Public/pz2-neuerwerbungen.css',
-			'useGoogleBooks' => '1',
-			'useZDB' => '1',
-			'subjects' => '',
-		);
+	protected function defaultSettings () {
+		$defaults = parent::defaultSettings();
 
-		foreach ( $defaults as $key => $value ) {
-			// If a setting is present and non-empty, use it. Otherwise use the default value.
-			if( $this->settings[$key] !== null && $this->settings[$key] !== '' ) {
-				$this->conf[$key] = $this->settings[$key];
-			} else {
-				$this->conf[$key] = $value;
-			}
-		}
+		$defaults['pz2-neuerwerbungenJSPath'] = t3lib_extMgm::siteRelPath('pazpar2neuerwerbungen') . 'Resources/Public/pz2-neuerwerbungen.js';
+		$defaults['pz2-neuerwerbungenCSSPath'] = t3lib_extMgm::siteRelPath('pazpar2neuerwerbungen') . 'Resources/Public/pz2-neuerwerbungen.css';
+		$defaults['subjects'] = '';
+
+		return $defaults;
 	}
 
 
+	
 
 	/**
 	 * Index: Insert pazpar2 CSS <link> and JavaScript <script>-tags into
@@ -66,7 +57,8 @@ class Tx_Pazpar2neuerwerbungen_Controller_Pazpar2neuerwerbungenController extend
 	 * @return void
 	 */
 	public function indexAction () {
-		$this->addResourcesToHead();
+		parent::indexAction();
+
 		$pz2Neuerwerbungen = new Tx_Pazpar2neuerwerbungen_Domain_Model_Pazpar2neuerwerbungen;
 		$pz2Neuerwerbungen->setSubjects( $this->getSubjectsArray() );
 		$pz2Neuerwerbungen->setMonths( $this->monthsArray(12) );
@@ -142,14 +134,8 @@ class Tx_Pazpar2neuerwerbungen_Controller_Pazpar2neuerwerbungenController extend
 	 *
 	 * @return void
 	 */
-	private function addResourcesToHead () {
-		// Add pazpar2.css to <head>.
-		$cssTag = new Tx_Fluid_Core_ViewHelper_TagBuilder('link');
-		$cssTag->addAttribute('rel', 'stylesheet');
-		$cssTag->addAttribute('type', 'text/css');
-		$cssTag->addAttribute('href', $this->conf['CSSPath']);
-		$cssTag->addAttribute('media', 'all');
-		$this->response->addAdditionalHeaderData( $cssTag->render() );
+	protected function addResourcesToHead () {
+		parent::addResourcesToHead();
 
 		// Add pz2-neuerwerbungen.css to <head>.
 		$cssTag = new Tx_Fluid_Core_ViewHelper_TagBuilder('link');
@@ -159,29 +145,6 @@ class Tx_Pazpar2neuerwerbungen_Controller_Pazpar2neuerwerbungenController extend
 		$cssTag->addAttribute('media', 'all');
 		$this->response->addAdditionalHeaderData( $cssTag->render() );
 
-
-		// Add pz2.js to <head>.
-		// This is Indexdata’s JavaScript that ships with the pazpar2 software.
-		$scriptTag = new Tx_Fluid_Core_ViewHelper_TagBuilder('script');
-		$scriptTag->addAttribute('type', 'text/javascript');
-		$scriptTag->addAttribute('src',  $this->conf['pz2JSPath']);
-		$scriptTag->forceClosingTag(true);
-		$this->response->addAdditionalHeaderData( $scriptTag->render() );
-
-		// Set up pazpar2 service ID.
-		$jsCommand = 'my_serviceID = "' . $this->conf['serviceID'] . '";';
-		$scriptTag = new Tx_Fluid_Core_ViewHelper_TagBuilder('script');
-		$scriptTag->addAttribute('type', 'text/javascript');
-		$scriptTag->setContent($jsCommand);
-		$this->response->addAdditionalHeaderData( $scriptTag->render() );
-
-		// Add pz2-client.js to <head>.
-		$scriptTag = new Tx_Fluid_Core_ViewHelper_TagBuilder('script');
-		$scriptTag->addAttribute('type', 'text/javascript');
-		$scriptTag->addAttribute('src', $this->conf['pz2-clientJSPath']) ;
-		$scriptTag->forceClosingTag(true);
-		$this->response->addAdditionalHeaderData( $scriptTag->render() );
-
 		// Add pz2-neuerwerbungen.js to <head>. ***************
 		$scriptTag = new Tx_Fluid_Core_ViewHelper_TagBuilder('script');
 		$scriptTag->addAttribute('type', 'text/javascript');
@@ -189,46 +152,12 @@ class Tx_Pazpar2neuerwerbungen_Controller_Pazpar2neuerwerbungenController extend
 		$scriptTag->forceClosingTag(true);
 		$this->response->addAdditionalHeaderData( $scriptTag->render() );
 
-
-
-		// Add settings for pz2-client.js to <head>.
-		$jsCommand = 'useGoogleBooks = ' . (($this->conf['useGoogleBooks']) ? 'true' : 'false') . '; ';
-		$jsCommand .= 'useZDB = ' . (($this->conf['useZDB']) ? 'true' : 'false') . ';';
-		$scriptTag = new Tx_Fluid_Core_ViewHelper_TagBuilder('script');
-		$scriptTag->addAttribute('type', 'text/javascript');
-		$scriptTag->setContent($jsCommand);
-		$this->response->addAdditionalHeaderData( $scriptTag->render() );
-
-		// Make jQuery initialise pazpar2 when the DOM is ready.
-		$jsCommand = 'jQuery(document).ready(domReady);';
-		$scriptTag = new Tx_Fluid_Core_ViewHelper_TagBuilder('script');
-		$scriptTag->addAttribute('type', 'text/javascript');
-		$scriptTag->setContent($jsCommand);
-		$this->response->addAdditionalHeaderData( $scriptTag->render() );
-
-		// Make jQuery initialise pazpar2 when the DOM is ready.
+		// Make jQuery initialise pazpar2neuerwerbungen when the DOM is ready.
 		$jsCommand = 'jQuery(document).ready(pz2neuerwerbungenDOMReady);';
 		$scriptTag = new Tx_Fluid_Core_ViewHelper_TagBuilder('script');
 		$scriptTag->addAttribute('type', 'text/javascript');
 		$scriptTag->setContent($jsCommand);
 		$this->response->addAdditionalHeaderData( $scriptTag->render() );
-
-		// Add Google Books support if asked to do so.
-		if ( $this->conf['useGoogleBooks'] ) {
-			// Structurally this might be better in a separate extension?
-			$scriptTag = new Tx_Fluid_Core_ViewHelper_TagBuilder('script');
-			$scriptTag->addAttribute('type', 'text/javascript');
-			$scriptTag->addAttribute('src',  'https://www.google.com/jsapi');
-			$scriptTag->forceClosingTag(true);
-			$this->response->addAdditionalHeaderData( $scriptTag->render() );
-			
-			$jsCommand = 'google.load("books", "0");';
-			$scriptTag = new Tx_Fluid_Core_ViewHelper_TagBuilder('script');
-			$scriptTag->addAttribute('type', 'text/javascript');
-			$scriptTag->setContent($jsCommand);
-			$this->response->addAdditionalHeaderData( $scriptTag->render() );
-		}
-
 	}
 
 
