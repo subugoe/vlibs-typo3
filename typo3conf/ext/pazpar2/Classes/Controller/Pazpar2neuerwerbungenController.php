d4857b5203e9d62d055fb364e5cd775ca55b2ead
ssp <porst@sub.uni-goettingen.de>
2011-05-03 17:28:34 +0200

    pazpar2neuewerbungen refactoring: move some functions controller->model

--- typo3conf/ext/pazpar2/Classes/Controller/Pazpar2neuerwerbungenController.php
+++ typo3conf/ext/pazpar2/Classes/Controller/Pazpar2neuerwerbungenController.php
@@ -66,7 +66,8 @@ class Tx_Pazpar2_Controller_Pazpar2neuerwerbungenController extends Tx_Pazpar2_C
 		parent::initializeAction();
 		
 		$this->pz2Neuerwerbungen = t3lib_div::makeInstance('Tx_Pazpar2_Domain_Model_Pazpar2neuerwerbungen');
-		$this->pz2Neuerwerbungen->setMonths($this->monthsArray());
+		$this->pz2Neuerwerbungen->setRootPPN($this->conf['neuerwerbungen-subjects']);
+		$this->pz2Neuerwerbungen->setRequestArguments($this->request->getArguments());
 	}
 	
 	
@@ -80,144 +81,11 @@ class Tx_Pazpar2_Controller_Pazpar2neuerwerbungenController extends Tx_Pazpar2_C
 	 * @return void
 	 */
 	public function indexAction () {
-		$this->setupSubjects();
 		$this->setupQueryString();
 		
 		parent::indexAction();
 
 		$this->view->assign('pazpar2neuerwerbungen', $this->pz2Neuerwerbungen);
-	}
-
-
-	
-	/**
-	 * Get the stored subjects array and add information about the selected ones
-	 *  from our arguments to it and set the model object up to use it.
-	 * 
-	 * If all child elements in a subject group are selected, also select the
-	 *	group itself.
-	 * 
-	 * If a subject group is selected, also select all the subjects contained in
-	 *  it. (If a subject group is not selected, _don't_ deselect all the
-	 *  children. Imperfect but probably the most reasonable thing to be done in
-	 *  a non-interactive setup like this one.)
-	 * 
-	 * @return void
-	 */
-	private function setupSubjects () {
-		$rootGOK = $this->conf['neuerwerbungen-subjects'];
-		$subjects = $this->getSubjectsArray($rootGOK);
-
-		// Figure out selected subjects from arguments. Subject argument names
-		// are "pz2subject-x0-x1-x2-..." where xi is the index of the subject group
-		// at level i.
-		$arguments = $this->request->getArguments();
-		if ($arguments['button']) {
-			$selectedCheckboxes = Array();
-			// Our form was submitted: use form values only.
-			foreach ($arguments as $argumentName => $argument) {
-				$fieldNameStart = 'pz2subject-';
-				if (($argument != '') && (strpos($argumentName, $fieldNameStart) === 0)) {
-					$nameParts = explode('-', substr($argumentName, strlen($fieldNameStart)));
-					if (count($nameParts) > 0) {
-						$mySubjects =& $subjects;
-						while (count($nameParts) > 1) {
-							$subjectIndex = intval(array_shift($nameParts));
-							$mySubjects =& $mySubjects[$subjectIndex]['subjects'];
-						}
-
-						$subjectIndex = intval(array_shift($nameParts));
-						$subject =& $mySubjects[$subjectIndex];
-						$subject['selected'] = True;
-						$selectedCheckboxes[] = implode(',', $subject['GOKs']);
-					}
-				}
-			}
-
-			// Also write the selected values to our cookie.
-			$cookieString = implode(':', $selectedCheckboxes);
-			setcookie('pz2neuerwerbungen-previousQuery', $cookieString);
-		}
-		else {
-			// Our form was not submitted: use cookie to set the selected checkboxes.
-			$previousQuery = $_COOKIE['pz2neuerwerbungen-previousQuery'];
-			$queryItems = explode(':', $previousQuery);
-
-			// Turn on the selection for each group if its GOKs have been passed
-			// in the cookie xor for each included subject if its GOKs have been
-			// passed in the cookie.
-			foreach ($subjects as &$group) {
-				$GOKsString = implode(',', $group['GOKs']);
-				if (in_array($GOKsString, $queryItems)) {
-					$group['selected'] = True;
-				}
-				else {
-					foreach ($group['subjects'] as &$subject) {
-						$GOKsString = implode(',', $subject['GOKs']);
-						if (in_array($GOKsString, $queryItems)) {
-							$subject['selected'] = True;
-						}
-					}
-				}
-			}
-		}
-
-		// Turn on the selection for the group if all containing subjects are selected
-		foreach ($subjects as &$group) {
-			$this->turnOnGroupSelectionIfNeeded($group);
-		}
-		
-		// Turn on the selection for all included subjects if the containing group is selected.
-		foreach ($subjects as &$group) {
-			$this->turnOnChildSelectionIfNeeded($group);
-		}
-
-		$this->pz2Neuerwerbungen->setSubjects($subjects);
-	}
-
-
-
-	/**
-	 * Takes the passed $group array and sets its 'selected' field to True if the 'selected' fields
-	 * of all the objects in its 'subjects' array are set to True. Uses recursion to check
-	 * potentially existant nested groups.
-	 *
-	 * @param array $group (passed by reference)
-	 * @return void
-	 */
-	private function turnOnGroupSelectionIfNeeded (&$group) {
-		$isSelected = True;
-		foreach ($group['subjects'] as &$subject) {
-			if ($subject['subjects']) {
-				$this->turnOnGroupSelectionIfNeeded($subject);
-			}
-			$isSelected &= $subject['selected'];
-		}
-
-		if ($isSelected) {
-			$group['selected'] = True;
-		}
-	}
-
-
-
-	/**
-	 * Checks whether the 'selected' field of the passed $group array is true and recursively sets
-	 * the 'selected' fields of all contained subjects in the 'subject' element to True if that is
-	 * the case.
-	 *
-	 * @param array $group (passed by reference)
-	 * @return void
-	 */
-	private function turnOnChildSelectionIfNeeded (&$group) {
-		if ($group['selected'] == True) {
-			foreach ($group['subjects'] as &$subject) {
-				$subject['selected'] = True;
-				if ($subject['subjects']) {
-					$this->turnOnChildSelectionIfNeeded($subject);
-				}
-			}
-		}
 	}
 
 
@@ -287,7 +155,7 @@ class Tx_Pazpar2_Controller_Pazpar2neuerwerbungenController extends Tx_Pazpar2_C
 		// If there is no selection, use the previous month (i.e. the item at
 		// index 0 of the monthsArray() result).
 		if ($months[0] == '') {
-			$monthKeysArray = array_keys($this->monthsArray());
+			$monthKeysArray = array_keys($this->pz2Neuerwerbungen->monthsArray());
 			$months = Array($monthKeysArray[1]);
 		}
 
@@ -357,183 +225,9 @@ class Tx_Pazpar2_Controller_Pazpar2neuerwerbungenController extends Tx_Pazpar2_C
 			}
 		}
 	}
+
+
 	
-	
-	
-	/**
-	 * reduceMonth: Assume the passed references to month and year numbers
-	 *	indicate a month; reduce them to indicate the previous month.
-	 *
-	 * @param $month reference to month number
-	 * @param $year reference to year number
-	 * @return void
-	 */
-	private function reduceMonth (&$month, &$year) {
-		if ($month == 1) {
-			$month = 12;
-			$year--;
-		}
-		else {
-			$month--;
-		}
-	}
-
-
-
-	/**
-	 * Return search string for Pica format of the given month: YYYYMM
-	 *
-	 * @param $month month number
-	 * @param $year year number
-	 * @return string the given month in YYYYMM format
-	 */
-	private function picaSearchStringForMonth ($month, $year) {
-		$leadingZero = '';
-		
-		if ($month < 10) {
-			$leadingZero = '0';
-		}
-		
-		return $year . $leadingZero . $month;
-	}
-
-
-
-	/**
-	 * Returns array of months preceding the current one.
-	 *	* Keys are of the form YYYYMM.
-	 *	* Values are localised names of the months followed by the year.
-	 *		Localised '(incomplete)' is appended to the name of the current month.
-	 *
-	 * @param $numberOfMonths (default = 13)
-	 * @return Array
-	 */
-	private function monthsArray ($numberOfMonths = 13) {
-		$months = array();
-		$year = date('Y');
-		$month = date('n');
-		
-		for ($i = 1; $i <= $numberOfMonths; $i++) {
-			$searchString = $this->picaSearchStringForMonth($month, $year);
-			
-			/* make sure the text encoding in the locale_all setting matches the encoding 
-					of the page, otherwise umlauts in month names may appear broken */  
-			$monthName = strftime('%B', mktime(0, 0, 0, $month, 1, 2010));
-			$displayString = $monthName . ' ' . $year;
-
-			if ($i == 1) {
-				$displayString .= ' (' . Tx_Extbase_Utility_Localization::translate('unvollständig', 'pazpar2') . ')';
-			}
-
-			$months[$searchString] = $displayString;
-
-			$this->reduceMonth($month, $year);
-		}
-		
-		return $months;
-	}
-
-
-
-	/**
-	 * Return array of subjects for the parentPPN passed.
-	 * The data needed are loaded from the tx_nkwgok_data table of the database.
-	 * They are expected to be imported from CSV-data by the GOK Plug-In. See its documentation
-	 * or code for the fields required in teh CSV-file.
-	 * The information is converted to nested arrays as required by the 'neuerwerbungen-form'
-	 * Partial that handles the display. The data format is:
-	 *	* Array [subject groups]
-	 *		* Array [subject group, associative]
-	 *			* name => string - name of subject group [required]
-	 *			* GOKs => Array [optional]
-	 *				* string that is a truncated GOK notation
-	 *			* subjects => Array [required, subjects]
-	 *				* Array [subject, associative]
-	 *					* name => string - name of subject group [required]
-	 *					* GOKs => Array [required]
-	 *						* string that is a truncated GOK notation
-	 *					* inline => boolean - displayed in one line with other items?
-	 *						[optional, defaults to false]
-	 *					* break => boolean - insert <br> before current element?
-	 *						[optional, should only be used with inline => true, defaults to false]
-	 *
-	 * If the GOKs field of a subject group is not specified, create it by taking
-	 *	the union of the GOKs arrays of all its subjects.
-	 *
-	 * @param string $parentPPN
-	 * @return Array subjects to be displayed
-	 */
-	private function getSubjectsArray ($parentPPN) {
-		$rootNodes = $this->queryForChildrenOf($parentPPN);
-		$subjects = array();
-
-		while($nodeRecord = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($rootNodes)) {
-			$subject = array();
-			$subject['name'] = $nodeRecord['descr']; // TODO: localise
-
-			// Recursively add child elements if they exist.
-			if ($nodeRecord['childcount'] > 0) {
-				$subject['subjects'] = $this->getSubjectsArray($nodeRecord['ppn']);
-			}
-
-			// Extract each search term from the 'search' field and add an array with all of them.
-			if ($nodeRecord['search'] != '') {
-				$searchComponents = array();
-				foreach (explode( ' or ', urldecode($nodeRecord['search'])) as $searchComponent) {
-					$component = trim($searchComponent, ' *');
-					$component = preg_replace('/^LKL /', '', $component);
-					$searchComponents[] = trim($component);
-				}
-				$subject['GOKs'] = $searchComponents;
-			}
-			else {
-				$subGOKs = array();
-				foreach ($subject['subjects'] as $subsubject) {
-					foreach ($subsubject['GOKs'] as $subGOK) {
-						$subGOKs[] = $subGOK;
-					}
-				}
-				$subject['GOKs'] = $subGOKs;
-			}
-
-			// Add tag fields to subject (inline and break).
-			foreach (explode(',', $nodeRecord['tags']) as $tag) {
-				if ($tag != '') {
-					$subject[$tag] = True;
-				}
-			}
-
-			$subjects[] = $subject;
-		}
-
-		return $subjects;
-	}
-
-
-
-	/**
-	 * Queries the database for all records having the $parentGOK parameter as their parent element
-	 *  and returns the query result.
-	 *
-	 * Uses table tx_nkwgok_data from the GOK extension.
-	 *
-	 * @param string $parentGOK
-	 * @return array
-	 */
-	private function queryForChildrenOf ($parentGOK) {
-		$queryResults = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
-			'*',
-			'tx_nkwgok_data',
-			"parent = '" . $parentGOK . "'",
-			'',
-			'gok ASC',
-			'');
-
-		return $queryResults;
-	}
-
-
-
 	/**
 	 * Inserts headers into page: first general ones by the superclass,
 	 *	then our own.
@@ -565,7 +259,6 @@ class Tx_Pazpar2_Controller_Pazpar2neuerwerbungenController extends Tx_Pazpar2_C
 		$scriptTag->setContent($jsCommand);
 		$this->response->addAdditionalHeaderData( $scriptTag->render() );
 	}
-
 
 }
 ?>
--- typo3conf/ext/pazpar2/Classes/Domain/Model/Pazpar2neuerwerbungen.php
+++ typo3conf/ext/pazpar2/Classes/Domain/Model/Pazpar2neuerwerbungen.php
@@ -31,29 +31,297 @@ class Tx_Pazpar2_Domain_Model_Pazpar2neuerwerbungen extends Tx_Extbase_DomainObj
 
 
 	/**
+	 * PPN (i.e. an ID) of the root element used for the Neuerwerbungen subject list.
+	 * A record with this value in the 'ppn' field should be in the tx_nkwgok_data table.
+	 * @var string
+	 */
+	protected $rootPPN;
+
+	/**
+	 * @return string
+	 */
+	public function getRootPPN () {
+		return $this->rootPPN;
+	}
+
+	/**
+	 * @param string $newRootGOK
+	 * @return void
+	 */
+	public function setRootPPN ($newRootPPN) {
+		$this->rootPPN = $newRootPPN;
+	}
+
+
+
+	/**
+	 * Stores the request’s arguments needed to determine the parameters submitted by the user.
+	 * @var array
+	 */
+	protected $requestArguments;
+
+	/**
+	 * @return array
+	 */
+	public function getRequestArguments () {
+		return $this->requestArguments;
+	}
+
+	/**
+	 * @param array $newRequestArguments
+	 * @return void
+	 */
+	public function setRequestArguments ($newRequestArguments) {
+		$this->requestArguments = $newRequestArguments;
+	}
+
+
+
+	/**
 	 * Array with subject tree.
-	 *
 	 * @var Array
 	 */
 	protected $subjects;
 	
 	/**
-	 * Getter for subject tree array.
-	 *
-	 * @return Array
+	 * @return array
 	 */
 	public function getSubjects () {
+		if ($this->subjects == Null) {
+			$this->setupSubjects();
+		}
+
 		return $this->subjects;
 	}
-	
+
+
+
 	/**
-	 * Setter for subject tree array.
+	 * Return array of subjects for the parentPPN passed.
+	 * The data needed are loaded from the tx_nkwgok_data table of the database.
+	 * They are expected to be imported from CSV-data by the GOK Plug-In. See its documentation
+	 * or code for the fields required in teh CSV-file.
 	 *
-	 * @param Array $newSubjects
+	 * The information is converted to nested arrays as required by the 'neuerwerbungen-form'
+	 * Partial that handles the display. The data format is:
+	 *	* Array [subject groups]
+	 *		* Array [subject group, associative]
+	 *			* name => string - name of subject group [required]
+	 *			* GOKs => Array [optional]
+	 *				* string that is a truncated GOK notation
+	 *			* subjects => Array [required, subjects]
+	 *				* Array [subject, associative]
+	 *					* name => string - name of subject group [required]
+	 *					* GOKs => Array [required]
+	 *						* string that is a truncated GOK notation
+	 *					* inline => boolean - displayed in one line with other items?
+	 *						[optional, defaults to false]
+	 *					* break => boolean - insert <br> before current element?
+	 *						[optional, should only be used with inline => true, defaults to false]
+	 *
+	 * If the GOKs field of a subject group is not specified, create it by taking
+	 *	the union of the GOKs arrays of all its subjects.
+	 *
+	 * @param string $parentPPN
+	 * @return array subjects to be displayed
+	 */
+	private function makeSubjectsArrayForPPN ($parentPPN) {
+		$rootNodes = $this->queryForChildrenOf($parentPPN);
+		$subjects = array();
+
+		while($nodeRecord = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($rootNodes)) {
+			$subject = array();
+			$subject['name'] = $nodeRecord['descr']; // TODO: localise
+
+			// Recursively add child elements if they exist.
+			if ($nodeRecord['childcount'] > 0) {
+				$subject['subjects'] = $this->makeSubjectsArrayForPPN($nodeRecord['ppn']);
+			}
+
+			// Extract each search term from the 'search' field and add an array with all of them.
+			if ($nodeRecord['search'] != '') {
+				$searchComponents = array();
+				foreach (explode( ' or ', urldecode($nodeRecord['search'])) as $searchComponent) {
+					$component = trim($searchComponent, ' *');
+					$component = preg_replace('/^LKL /', '', $component);
+					$searchComponents[] = trim($component);
+				}
+				$subject['GOKs'] = $searchComponents;
+			}
+			else {
+				$subGOKs = array();
+				foreach ($subject['subjects'] as $subsubject) {
+					foreach ($subsubject['GOKs'] as $subGOK) {
+						$subGOKs[] = $subGOK;
+					}
+				}
+				$subject['GOKs'] = $subGOKs;
+			}
+
+			// Add tag fields to subject (inline and break).
+			foreach (explode(',', $nodeRecord['tags']) as $tag) {
+				if ($tag != '') {
+					$subject[$tag] = True;
+				}
+			}
+
+			$subjects[] = $subject;
+		}
+
+		return $subjects;
+	}
+
+
+
+	/**
+	 * Queries the database for all records having the $parentPPN parameter as their parent element
+	 *  and returns the query result.
+	 *
+	 * Uses table tx_nkwgok_data from the GOK extension.
+	 *
+	 * @param string $parentPPN
+	 * @return array
+	 */
+	private function queryForChildrenOf ($parentPPN) {
+		$queryResults = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+			'*',
+			'tx_nkwgok_data',
+			"parent = '" . $parentPPN . "'",
+			'',
+			'gok ASC',
+			'');
+
+		return $queryResults;
+	}
+
+
+
+	/**
+	 * Get the subjects array, add information about the selected ones
+	 *  from our arguments to it, and store it.
+	 *
+	 * If all child elements in a subject group are selected, also select the
+	 *	group itself.
+	 *
+	 * If a subject group is selected, also select all the subjects contained in
+	 *  it. (If a subject group is not selected, _don't_ deselect all the
+	 *  children. Imperfect but probably the most reasonable thing to be done in
+	 *  a non-interactive setup like this one.)
+	 *
 	 * @return void
 	 */
-	public function setSubjects ($newSubjects) {
-		$this->subjects = $newSubjects;
+	private function setupSubjects () {
+		$subjects = $this->makeSubjectsArrayForPPN($this->rootPPN);
+
+		// Figure out selected subjects from the request’s arguments. Subject argument names
+		// are "pz2subject-x0-x1-x2-..." where xi is the index of the subject group
+		// at level i.
+		if ($this->requestArguments['button']) {
+			$selectedCheckboxes = Array();
+			// Our form was submitted: use form values only.
+			foreach ($this->requestArguments as $argumentName => $argument) {
+				$fieldNameStart = 'pz2subject-';
+				if (($argument != '') && (strpos($argumentName, $fieldNameStart) === 0)) {
+					$nameParts = explode('-', substr($argumentName, strlen($fieldNameStart)));
+					if (count($nameParts) > 0) {
+						$mySubjects =& $subjects;
+						while (count($nameParts) > 1) {
+							$subjectIndex = intval(array_shift($nameParts));
+							$mySubjects =& $mySubjects[$subjectIndex]['subjects'];
+						}
+
+						$subjectIndex = intval(array_shift($nameParts));
+						$subject =& $mySubjects[$subjectIndex];
+						$subject['selected'] = True;
+						$selectedCheckboxes[] = implode(',', $subject['GOKs']);
+					}
+				}
+			}
+
+			// Also write the selected values to our cookie.
+			$cookieString = implode(':', $selectedCheckboxes);
+			setcookie('pz2neuerwerbungen-previousQuery', $cookieString);
+		}
+		else {
+			// Our form was not submitted: use cookie to set the selected checkboxes.
+			$previousQuery = $_COOKIE['pz2neuerwerbungen-previousQuery'];
+			$queryItems = explode(':', $previousQuery);
+
+			// Turn on the selection for each group if its GOKs have been passed
+			// in the cookie xor for each included subject if its GOKs have been
+			// passed in the cookie.
+			foreach ($subjects as &$group) {
+				$GOKsString = implode(',', $group['GOKs']);
+				if (in_array($GOKsString, $queryItems)) {
+					$group['selected'] = True;
+				}
+				else {
+					foreach ($group['subjects'] as &$subject) {
+						$GOKsString = implode(',', $subject['GOKs']);
+						if (in_array($GOKsString, $queryItems)) {
+							$subject['selected'] = True;
+						}
+					}
+				}
+			}
+		}
+
+		// Turn on the selection for the group if all containing subjects are selected
+		foreach ($subjects as &$group) {
+			$this->turnOnGroupSelectionIfNeeded($group);
+		}
+
+		// Turn on the selection for all included subjects if the containing group is selected.
+		foreach ($subjects as &$group) {
+			$this->turnOnChildSelectionIfNeeded($group);
+		}
+
+		$this->subjects = $subjects;
+	}
+
+
+
+	/**
+	 * Takes the passed $group array and sets its 'selected' field to True if the 'selected' fields
+	 * of all the objects in its 'subjects' array are set to True. Uses recursion to check
+	 * potentially existant nested groups.
+	 *
+	 * @param array $group (passed by reference)
+	 * @return void
+	 */
+	private function turnOnGroupSelectionIfNeeded (&$group) {
+		$isSelected = True;
+		foreach ($group['subjects'] as &$subject) {
+			if ($subject['subjects']) {
+				$this->turnOnGroupSelectionIfNeeded($subject);
+			}
+			$isSelected &= $subject['selected'];
+		}
+
+		if ($isSelected) {
+			$group['selected'] = True;
+		}
+	}
+
+
+
+	/**
+	 * Checks whether the 'selected' field of the passed $group array is true and recursively sets
+	 * the 'selected' fields of all contained subjects in the 'subject' element to True if that is
+	 * the case.
+	 *
+	 * @param array $group (passed by reference)
+	 * @return void
+	 */
+	private function turnOnChildSelectionIfNeeded (&$group) {
+		if ($group['selected'] == True) {
+			foreach ($group['subjects'] as &$subject) {
+				$subject['selected'] = True;
+				if ($subject['subjects']) {
+					$this->turnOnChildSelectionIfNeeded($subject);
+				}
+			}
+		}
 	}
 
 
@@ -65,27 +333,99 @@ class Tx_Pazpar2_Domain_Model_Pazpar2neuerwerbungen extends Tx_Extbase_DomainObj
 	 *	* searchTerms - string used to query the catalogue’s DTM field, e.g. 201010.
 	 * 		In case several months are used, the different strings are comma separated.
 	 *
-	 * @var Array
+	 * @var array
 	 */
 	protected $months;
 
 	/**
-	 * Getter for months array.
-	 *
 	 * @return Array
 	 */
 	public function getMonths () {
+		if ($this->months == Null) {
+			$this->months = $this->monthsArray();
+		}
 		return $this->months;
 	}
 	
 	/**
-	 * Setter for months array.
-	 *
 	 * @param Array $newMonths
 	 * @return void
 	 */
 	public function setMonths ($newMonths) {
 		$this->months = $newMonths;
+	}
+
+	/**
+	 * Returns array of months preceding the current one.
+	 *	* Keys are of the form YYYYMM.
+	 *	* Values are localised names of the months followed by the year.
+	 *		Localised '(incomplete)' is appended to the name of the current month.
+	 *
+	 * @param $numberOfMonths (default = 13)
+	 * @return Array
+	 */
+	public function monthsArray ($numberOfMonths = 13) {
+		$months = array();
+		$year = date('Y');
+		$month = date('n');
+
+		for ($i = 1; $i <= $numberOfMonths; $i++) {
+			$searchString = $this->picaSearchStringForMonth($month, $year);
+
+			/* make sure the text encoding in the locale_all setting matches the encoding
+					of the page, otherwise umlauts in month names may appear broken */
+			$monthName = strftime('%B', mktime(0, 0, 0, $month, 1, 2010));
+			$displayString = $monthName . ' ' . $year;
+
+			if ($i == 1) {
+				$displayString .= ' (' . Tx_Extbase_Utility_Localization::translate('unvollständig', 'pazpar2') . ')';
+			}
+
+			$months[$searchString] = $displayString;
+
+			$this->reduceMonth($month, $year);
+		}
+
+		return $months;
+	}
+
+
+
+	/**
+	 * reduceMonth: Assume the passed references to month and year numbers
+	 *	indicate a month; reduce them to indicate the previous month.
+	 *
+	 * @param $month reference to month number
+	 * @param $year reference to year number
+	 * @return void
+	 */
+	private function reduceMonth (&$month, &$year) {
+		if ($month == 1) {
+			$month = 12;
+			$year--;
+		}
+		else {
+			$month--;
+		}
+	}
+
+
+
+	/**
+	 * Return search string for Pica format of the given month: YYYYMM
+	 *
+	 * @param $month month number
+	 * @param $year year number
+	 * @return string the given month in YYYYMM format
+	 */
+	private function picaSearchStringForMonth ($month, $year) {
+		$leadingZero = '';
+
+		if ($month < 10) {
+			$leadingZero = '0';
+		}
+
+		return $year . $leadingZero . $month;
 	}
 
 
