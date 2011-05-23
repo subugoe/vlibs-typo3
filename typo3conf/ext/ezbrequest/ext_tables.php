0083304e16f8330556456505f8e97a67fa08a5bc
ssp <porst@sub.uni-goettingen.de>
2011-05-23 17:54:44 +0200

    ezbrequest extension from vifamath

---
+++ typo3conf/ext/ezbrequest/ext_tables.php
@@ -0,0 +1,15 @@
+<?php
+if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
+
+
+t3lib_extMgm::addPlugin(array('LLL:EXT:ezbrequest/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
+
+
+t3lib_extMgm::addStaticFile($_EXTKEY,"static/","ezbrequest");
+
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] ='pi_flexform';
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY . '/flexform.xml');
+?>
