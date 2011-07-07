f208a99c8518f85578944a7e7d5a4231386e6a17
ssp <porst@sub.uni-goettingen.de>
2011-07-07 17:17:10 +0200

    TYPO3 site setup

--- typo3conf/localconf.php
+++ typo3conf/localconf.php
@@ -1,5 +1,5 @@
 <?php
-$TYPO3_CONF_VARS['SYS']['sitename'] = 'New TYPO3 site';
+$TYPO3_CONF_VARS['SYS']['sitename'] = 'GEO-LEO';
 
 	// Default password is "joh316" :
 $TYPO3_CONF_VARS['BE']['installToolPassword'] = 'bacb98acf97e0b6112b1d1b650b84971';
@@ -13,17 +13,17 @@ $TYPO3_CONF_VARS['SYS']['devIPmask'] = '127.0.0.1,::1,134.76.162.165';
 
 ## INSTALL SCRIPT EDIT POINT TOKEN - all lines after this points may be changed by the install script!
 
-$TYPO3_CONF_VARS['EXT']['extList'] = 'extbase,css_styled_content,version,tsconfig_help,context_help,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,aboutmodules,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,t3skin,reports,realurl,cag_linkchecker,static_info_tables,cshmanual,recycler,fluid,beko_debugster,scheduler,t3editor,devlog,pazpar2,t3jquery,ezbrequest,kickstarter,nkwgok';	// Modified or inserted by TYPO3 Extension Manager. Modified or inserted by TYPO3 Core Update Manager. 
+$TYPO3_CONF_VARS['EXT']['extList'] = 'extbase,css_styled_content,version,tsconfig_help,context_help,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,aboutmodules,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,t3skin,reports,realurl,cag_linkchecker,static_info_tables,cshmanual,recycler,fluid,beko_debugster,scheduler,t3editor,devlog,pazpar2,t3jquery,ezbrequest,kickstarter,info,nkwgok';	// Modified or inserted by TYPO3 Extension Manager. Modified or inserted by TYPO3 Core Update Manager. 
 // Updated by TYPO3 Core Update Manager 08-09-10 12:29:22
 $TYPO3_CONF_VARS['SYS']['encryptionKey'] = 'eff43017a2a82314f23139d9daf0e129a20af589316ec80bd0084f268b67125e5a14a2b2a9e0527657b4059ebd41f44e';	// Modified or inserted by TYPO3 Install Tool. 
 $TYPO3_CONF_VARS['SYS']['compat_version'] = '4.5';	// Modified or inserted by TYPO3 Install Tool. 
 $typo_db_username = 'root';	//  Modified or inserted by TYPO3 Install Tool.
-$typo_db_password = 'DB4Typo3';	//  Modified or inserted by TYPO3 Install Tool.
+$typo_db_password = '';	//  Modified or inserted by TYPO3 Install Tool.
 $typo_db_host = 'localhost';	//  Modified or inserted by TYPO3 Install Tool.
 $TYPO3_CONF_VARS['EXTCONF']['dbal']['handlerCfg'] = array('_DEFAULT' => array('type' => 'adodb','config' => array('driver' => '',)));;	// Modified or inserted by TYPO3 Install Tool. 
 // Updated by TYPO3 Install Tool 08-09-10 12:44:18
 // Updated by TYPO3 Core Update Manager 08-09-10 12:54:50
-$typo_db = 'typo3';	//  Modified or inserted by TYPO3 Install Tool.
+$typo_db = 'typo3geoleo';	//  Modified or inserted by TYPO3 Install Tool.
 $TYPO3_CONF_VARS['BE']['installToolPassword'] = '7c68019aff42ab5929c42aea9a441bcd';	//  Modified or inserted by TYPO3 Install Tool.
 $TYPO3_CONF_VARS['BE']['disable_exec_function'] = '0';	//  Modified or inserted by TYPO3 Install Tool.
 $TYPO3_CONF_VARS['GFX']['gdlib_png'] = '1';	// Modified or inserted by TYPO3 Install Tool. 
@@ -31,7 +31,6 @@ $TYPO3_CONF_VARS['GFX']['im'] = '0';	//  Modified or inserted by TYPO3 Install T
 $TYPO3_CONF_VARS['GFX']['im_combine_filename'] = '';	//  Modified or inserted by TYPO3 Install Tool.
 $TYPO3_CONF_VARS['GFX']['im_path'] = '';	//  Modified or inserted by TYPO3 Install Tool.
 $TYPO3_CONF_VARS['GFX']['im_path_lzw'] = '';	//  Modified or inserted by TYPO3 Install Tool.
-$TYPO3_CONF_VARS['SYS']['sitename'] = 'VLib-Test';	//  Modified or inserted by TYPO3 Install Tool.
 // Updated by TYPO3 Install Tool 08-09-10 13:03:28
 $TYPO3_CONF_VARS['EXT']['extList_FE'] = 'extbase,css_styled_content,version,install,rtehtmlarea,t3skin,realurl,cag_linkchecker,static_info_tables,fluid,beko_debugster,devlog,pazpar2,t3jquery,ezbrequest,kickstarter,nkwgok';	// Modified or inserted by TYPO3 Extension Manager. 
 // Updated by TYPO3 Extension Manager 22-09-10 13:52:06
@@ -139,4 +138,5 @@ $TYPO3_CONF_VARS['EXT']['extConf']['nkwgok'] = 'a:2:{s:11:"opacBaseURL";s:39:"ht
 $TYPO3_CONF_VARS['EXT']['extConf']['smarty'] = 'a:7:{s:9:"IMPORTANT";s:1:"1";s:10:"smarty_dir";s:17:"EXT:smarty/Smarty";s:12:"template_dir";s:22:"fileadmin/admin/smarty";s:7:"caching";s:1:"0";s:16:"respect_no_cache";s:1:"0";s:13:"compile_check";s:1:"0";s:9:"debugging";s:1:"0";}';	// Modified or inserted by TYPO3 Extension Manager. 
 // Updated by TYPO3 Extension Manager 15-06-11 17:44:04
 // Updated by TYPO3 Install Tool 20-06-11 14:47:02
+// Updated by TYPO3 Extension Manager 07-07-11 15:02:30
 ?>
