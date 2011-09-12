f6a514361ea2f83ca42ecc744b46fd8960f567fa
ssp <porst@sub.uni-goettingen.de>
2011-09-12 13:50:13 +0200

    add language redirection & change 404 page
    Conflicts:
    
    	typo3conf/localconf.php

--- typo3conf/localconf.php
+++ typo3conf/localconf.php
@@ -1,11 +1,4 @@
 <?php
-$TYPO3_CONF_VARS['SYS']['sitename'] = 'New TYPO3 site';
-
-	// Default password is "joh316" :
-$TYPO3_CONF_VARS['BE']['installToolPassword'] = 'bacb98acf97e0b6112b1d1b650b84971';
-
-$TYPO3_CONF_VARS['EXT']['extList'] = 'version,tsconfig_help,context_help,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,aboutmodules,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,css_styled_content,t3skin,t3editor,reports';
-
 $typo_db_extTableDef_script = 'extTables.php';
 
 $TYPO3_CONF_VARS['SYS']['displayErrors'] = 2;
@@ -13,16 +6,12 @@ $TYPO3_CONF_VARS['SYS']['devIPmask'] = '127.0.0.1,::1,134.76.162.165';
 
 ## INSTALL SCRIPT EDIT POINT TOKEN - all lines after this points may be changed by the install script!
 
-$TYPO3_CONF_VARS['EXT']['extList'] = 'extbase,css_styled_content,version,tsconfig_help,context_help,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,aboutmodules,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,t3skin,reports,realurl,cag_linkchecker,static_info_tables,cshmanual,recycler,fluid,beko_debugster,scheduler,t3editor,devlog,pazpar2,t3jquery,ezbrequest,kickstarter,filelist,info,nkwgok';	// Modified or inserted by TYPO3 Extension Manager. Modified or inserted by TYPO3 Core Update Manager. 
 // Updated by TYPO3 Core Update Manager 08-09-10 12:29:22
 $TYPO3_CONF_VARS['SYS']['encryptionKey'] = 'eff43017a2a82314f23139d9daf0e129a20af589316ec80bd0084f268b67125e5a14a2b2a9e0527657b4059ebd41f44e';	// Modified or inserted by TYPO3 Install Tool. 
 $TYPO3_CONF_VARS['SYS']['compat_version'] = '4.5';	// Modified or inserted by TYPO3 Install Tool. 
 $typo_db_username = 'root';	//  Modified or inserted by TYPO3 Install Tool.
 $typo_db_password = '';	//  Modified or inserted by TYPO3 Install Tool.
 $typo_db_host = 'localhost';	//  Modified or inserted by TYPO3 Install Tool.
-$TYPO3_CONF_VARS['EXTCONF']['dbal']['handlerCfg'] = array('_DEFAULT' => array('type' => 'adodb','config' => array('driver' => '',)));;	// Modified or inserted by TYPO3 Install Tool. 
-// Updated by TYPO3 Install Tool 08-09-10 12:44:18
-// Updated by TYPO3 Core Update Manager 08-09-10 12:54:50
 $typo_db = 'typo3';	//  Modified or inserted by TYPO3 Install Tool.
 $TYPO3_CONF_VARS['BE']['installToolPassword'] = '7c68019aff42ab5929c42aea9a441bcd';	//  Modified or inserted by TYPO3 Install Tool.
 $TYPO3_CONF_VARS['BE']['disable_exec_function'] = '0';	//  Modified or inserted by TYPO3 Install Tool.
@@ -32,11 +21,14 @@ $TYPO3_CONF_VARS['GFX']['im_combine_filename'] = '';	//  Modified or inserted by
 $TYPO3_CONF_VARS['GFX']['im_path'] = '';	//  Modified or inserted by TYPO3 Install Tool.
 $TYPO3_CONF_VARS['GFX']['im_path_lzw'] = '';	//  Modified or inserted by TYPO3 Install Tool.
 $TYPO3_CONF_VARS['SYS']['sitename'] = 'VLib-Test';	//  Modified or inserted by TYPO3 Install Tool.
-// Updated by TYPO3 Install Tool 08-09-10 13:03:28
-$TYPO3_CONF_VARS['EXT']['extList_FE'] = 'extbase,css_styled_content,version,install,rtehtmlarea,t3skin,realurl,cag_linkchecker,static_info_tables,fluid,beko_debugster,devlog,pazpar2,t3jquery,ezbrequest,kickstarter,nkwgok';	// Modified or inserted by TYPO3 Extension Manager. 
-// Updated by TYPO3 Extension Manager 22-09-10 13:52:06
 
 $TYPO3_CONF_VARS['FE']['addRootLineFields'].= ',tx_realurl_pathsegment';
+$TYPO3_CONF_VARS['FE']['pageNotFound_handling'] = '404/';	// Modified or inserted by TYPO3 Install Tool.
+
+$TYPO3_CONF_VARS['EXT']['extList'] = 'extbase,css_styled_content,version,tsconfig_help,context_help,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,aboutmodules,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,t3skin,reports,realurl,cag_linkchecker,static_info_tables,cshmanual,recycler,fluid,beko_debugster,scheduler,t3editor,devlog,pazpar2,t3jquery,ezbrequest,kickstarter,filelist,info,nkwgok,perm,rlmp_language_detection';	// Modified or inserted by TYPO3 Extension Manager. Modified or inserted by TYPO3 Core Update Manager. 
+$TYPO3_CONF_VARS['EXT']['extList_FE'] = 'extbase,css_styled_content,version,install,rtehtmlarea,t3skin,realurl,cag_linkchecker,static_info_tables,fluid,beko_debugster,devlog,pazpar2,t3jquery,ezbrequest,kickstarter,nkwgok,rlmp_language_detection';	// Modified or inserted by TYPO3 Extension Manager.
+
+$TYPO3_CONF_VARS['EXTCONF']['dbal']['handlerCfg'] = array('_DEFAULT' => array('type' => 'adodb','config' => array('driver' => '',)));;	// Modified or inserted by TYPO3 Install Tool.
 
 $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT'] = array(
 	'init' => array (
@@ -138,5 +130,4 @@ $TYPO3_CONF_VARS['GFX']['im_combine_filename'] = '';	//  Modified or inserted by
 $TYPO3_CONF_VARS['GFX']['im_path'] = '';	//  Modified or inserted by TYPO3 Install Tool.
 $TYPO3_CONF_VARS['GFX']['im_path_lzw'] = '';	//  Modified or inserted by TYPO3 Install Tool.
 
-// Updated by TYPO3 Extension Manager 26-08-11 15:26:17
 ?>
