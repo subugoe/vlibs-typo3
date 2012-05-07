40e8dae777b0d4dfcce8a0769b42070ca4a9fb29
ssp <porst@sub.uni-goettingen.de>
2012-05-07 15:07:56 +0200

    remove extensions

--- typo3conf/ext/libconnect/ext_localconf.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined ('TYPO3_MODE')) {	die ('Access denied.'); }
-
-require_once(t3lib_extMgm::extPath('div') . 'class.tx_div.php');
-if(TYPO3_MODE == 'FE') tx_div::autoLoadAll($_EXTKEY);
-
-?>
--- typo3conf/ext/libconnect/views/class.tx_libconnect_views_smarty.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-tx_div::load('tx_lib_smartyView');
-
-class tx_libconnect_views_smarty extends tx_lib_smartyView {
-	var $extensionKey = "libconnect";
-}
-
-?>
