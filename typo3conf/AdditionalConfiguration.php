0ae2b661f33f98f934c14c4b3d842ad877b525d0
Sven-S. Porst <porst@sub.uni-goettingen.de>
2013-07-31 10:55:27 +0200

    fix RealURL configuration

--- typo3conf/AdditionalConfiguration.php
+++ typo3conf/AdditionalConfiguration.php
@@ -1,27 +1,27 @@
 <?php
-	$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT'] = array(
-		'init' => array(
-			'appendMissingSlash' => 'ifNotFile,redirect[301]',
-			'respectSimulateStaticURLs' => true,
-			'enableCHashCache' => true,
-			'enableUrlDecodeCache' => false,
-			'enableUrlEncodeCache' => false,
-		),
-		'preVars' => array(
-			array(
-				'GETvar' => 'L',
-				'valueMap' => array(
-					'en' => 2,
-				),
-				'noMatch' => 'bypass'
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT'] = array(
+	'init' => array(
+		'appendMissingSlash' => 'ifNotFile,redirect[301]',
+		'respectSimulateStaticURLs' => true,
+		'enableCHashCache' => true,
+		'enableUrlDecodeCache' => false,
+		'enableUrlEncodeCache' => false,
+	),
+	'preVars' => array(
+		array(
+			'GETvar' => 'L',
+			'valueMap' => array(
+				'en' => 2,
 			),
+			'noMatch' => 'bypass'
 		),
-		'pagePath' => array(
-			'type' => 'user',
-			'userFunc' => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
-			'spaceCharacter' => '-',
-			'languageGetVar' => 'L',
-			'rootpage_id' => '2',
-		)
-	);
+	),
+	'pagePath' => array(
+		'type' => 'user',
+		'userFunc' => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
+		'spaceCharacter' => '-',
+		'languageGetVar' => 'L',
+		'rootpage_id' => '2',
+	)
+);
 ?>
